63ec590c063f6b4931d13859cfe5b594621e94ad
Fredrik Omstedt <fredrik,omstedt@gmail.com>
2018-10-08 19:51:47 +0200

    Added CORS functionality

--- app/views.py
+++ app/views.py
@@ -6,11 +6,13 @@
 
 from app import app
 from flask import render_template, jsonify, request
+from flask_cors import cross_origin
 import traceback
 import psycopg2
 import psycopg2.extras
 
 # Routes the address to show the interface html file
 @app.route('/test')
+@cross_origin()
 def test():
     return jsonify({'test': 4})
