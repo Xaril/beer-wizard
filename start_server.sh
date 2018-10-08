66b4290cb25323a4603c38d3706a77361617923c
Fredrik Omstedt <fredrik,omstedt@gmail.com>
2018-10-08 19:24:03 +0200

    Changed server port to 8332 (BEER) :D

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,4 @@
 #Starts the interface, making it accessible at port 8085
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PYTHONPATH="${PYTHONPATH}:$DIR"
-./watchdog.sh python /usr/local/bin/twistd -n web --port "tcp:port=8085" --wsgi run_beer_wizard.app
+./watchdog.sh python /usr/local/bin/twistd -n web --port "tcp:port=8332" --wsgi run_beer_wizard.app
