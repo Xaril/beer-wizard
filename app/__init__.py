d4805ce2a20218607334a3063d32cce8305a6c14
Xaril <fredrik,omstedt@gmail.com>
2018-10-08 20:49:57 +0200

    Added database functionality in flask
    Also added two queries, getAllSpells and getAllPlayers

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+#CORS configuration
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+#MySQL configuration
+mysql = MySQL(app)
+app.config['MYSQL_USER'] = 'beerwizard'
+app.config['MYSQL_PASSWORD'] = 'B33rW1z4rd'
+app.config['MYSQL_DB'] = 'beer_wizard'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -4,15 +4,47 @@
 # Author: Fredrik Omstedt
 # Date: 2018-10-08
 
-from app import app
-from flask import render_template, jsonify, request
+from app import app, mysql
+from flask import jsonify, request
 from flask_cors import cross_origin
 import traceback
-import psycopg2
-import psycopg2.extras
 
-# Routes the address to show the interface html file
-@app.route('/test')
+# Retrieves all spells possible to cast
+@app.route('/api/get_all_spells')
 @cross_origin()
-def test():
-    return 200, jsonify({'test': 4})
+def get_all_spells():
+    status_code = 200
+    spells = []
+    try:
+        # Fetch the spells from the database
+        cur = mysql.connection.cursor()
+        cur.execute('''SELECT * FROM spell ORDER BY level ASC''')
+        results = cur.fetchall()
+
+        # Add the spells to a list
+        for result in results:
+            spells.append(result)
+    except:
+        traceback.print_exc()
+        status_code = 500
+    return jsonify({'spells': spells}), status_code
+
+# Retrieves all players in the game
+@app.route('/api/get_all_players')
+@cross_origin()
+def get_all_players():
+    status_code = 200
+    players = []
+    try:
+        # Fetch the players from the database
+        cur = mysql.connection.cursor()
+        cur.execute('''SELECT name, level FROM user''')
+        results = cur.fetchall()
+
+        # Add the spells to a list
+        for result in results:
+            players.append(result)
+    except:
+        traceback.print_exc()
+        status_code = 500
+    return jsonify({'players': players}), status_code
