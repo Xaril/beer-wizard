64096dbd1b5a8b674d764f3749140a7f8e63e016
Cristian Osorio Bretti <cristian.osoriobretti@gmail.com>
2018-10-08 23:11:41 +0200

    Now fetches players correctly

--- beer-wizard/src/components/AllPlayersView/ListOfPlayers.js
+++ beer-wizard/src/components/AllPlayersView/ListOfPlayers.js
@@ -3,85 +3,18 @@ import PlayersOnLevel from './PlayersOnLevel';
 import _ from 'lodash'
 import Header from '../../shared-components/Header';
 
-var players = [
-    {
-        name: "Fredrik",
-        level: 1
-    },
-    {
-        name: "Cristian",
-        level: 1
-    },
-    {
-        name: "Anton",
-        level: 3
-    },
-    {
-        name: "Erik",
-        level: 3
-    },
-    {
-        name: "Pelle",
-        level: 3
-    },
-    {
-        name: "Anton",
-        level: 3
-    },
-    {
-        name: "Erik",
-        level: 3
-    },
-    {
-        name: "Pelle",
-        level: 3
-    },
-    {
-        name: "Anton",
-        level: 3
-    },
-    {
-        name: "Erik",
-        level: 3
-    },
-    {
-        name: "Pelle",
-        level: 3
-    },
-    {
-        name: "Anton",
-        level: 4
-    },
-    {
-        name: "Erik",
-        level: 4
-    },
-    {
-        name: "Pelle",
-        level: 5
-    },
-    {
-        name: "Erik",
-        level: 5
-    },
-    {
-        name: "Pelle",
-        level: 5
-    },
-    
-
-]
 class ListOfPlayers extends Component {
     componentDidMount() {
-        console.log(this.props);
-        //this.props.load();
+        this.props.load();
     }
     render() {
         var loading = this.props.players.status === "UPDATING";
         if (loading){
             return <h3>FETCHING</h3>
         } else {
-            var sortedListObject = _.groupBy(players, "level");
+            console.log("HEERE")
+            var sortedListObject = _.groupBy(this.props.players.data.players, "level");
+            console.log(sortedListObject);
             return (
                 <div>
                     <Header header="Players"/>
