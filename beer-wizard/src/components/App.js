cf767794bedc819f002ce41d19d14c6a0f55301b
Cristian Osorio Bretti <cristian.osoriobretti@gmail.com>
2018-10-08 21:31:33 +0200

    Added the react router and three paths

--- beer-wizard/src/components/App.js
+++ beer-wizard/src/components/App.js
@@ -2,27 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ListOfPlayersContainer from './AllPlayersView/ListOfPlayersContainer';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+
+
 class App extends Component {
 
     render() {
         return (
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header>
-            <ListOfPlayersContainer />
-          </div>
+          <Router>
+            <div>
+              <Route exact path="/" component={ListOfPlayersContainer}/>
+              <Route path="/players" component={ListOfPlayersContainer}/>
+              <Route path="/spells" component={ListOfPlayersContainer}/>
+              
+              <div>
+                <Link to="/players">
+                  <button type="button">Players</button>
+                </Link>
+
+                <Link to="/">
+                  <button type="button">Home</button>
+                </Link>
+
+                <Link to="/spells">
+                  <button type="button">Spells</button>
+                </Link>
+              </div>
+              
+            </div>
+          </Router>
         );
     }
 }
