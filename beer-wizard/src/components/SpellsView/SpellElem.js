3a3a50167c1078041c7c74cc724d267b883d56af
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2018-10-09 11:16:33 +0200

    Added CSS for Spell list

--- beer-wizard/src/components/SpellsView/SpellElem.js
+++ beer-wizard/src/components/SpellsView/SpellElem.js
@@ -4,22 +4,16 @@ class SpellElem extends React.Component  {
     render() {
         const spell = this.props.spell;
         return (
-            <div style={{
-
-            }}>
+            <div style={{margin:' 2.5% 7.5%', backgroundColor:'#FFFFFF'}}>
                 <div style={{
                     textAlign: 'center',
                     fontSize: 22,
+                    margin: '1px', padding: '1%', color: '#000000', backgroundColor:'#b2285f'
                 }}>
-                    {spell.level}
+                    {"Level " + spell.level + " - " + spell.name}
                 </div>
-                <div style={{
-                    textAlign: 'center',
-                    fontSize: 20,
-                }}>
-                    {spell.name}
-                </div>
-                <div>
+
+                <div style={{textAlign: 'center', flex: 1, padding:'1%', margin:'1px',backgroundColor:'#e85d8c'}}>
                     {spell.description}
                 </div>
             </div>
